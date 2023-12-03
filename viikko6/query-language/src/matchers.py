3f5b484de83b8d49ca3ba494a1860025acb7f9b0
Juuso Hyyryläinen <juusohyyrylainen@hotmail.com>
2023-12-03 20:38:23 +0200

    viikko6

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,55 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All:
+
+    def test(self, players):
+        return True
+
+class Not:
+    def __init__(self, matcher):
+        self._matcher= matcher
+
+    def test(self, player):
+        return not self._matcher.test(player)
+
+ 
+class HasFewerThan:
+    def __init__(self, value,attr):
+        self._attr= attr
+        self._value = value
+
+    def test(self, player):
+        player_value = getattr(player,self._attr)
+        return player_value < self._value
+
+class Or:
+    def __init__(self,*matchers):
+        self._matchers = matchers
+
+    def test(self,player):
+        for matcher in self._matchers:
+            if matcher.test(player):
+                return True
+        return False
+    
+
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self._query = query
+
+    def playsIn(self,team):
+        return QueryBuilder(And(self._query, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._query, HasAtLeast(value,attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._query, HasFewerThan(value,attr)))
+    
+    def build(self):
+        return self._query
+    
+    def oneOf(self,m1,m2):
+        return QueryBuilder(And(self._query, Or(m1,m2)))
