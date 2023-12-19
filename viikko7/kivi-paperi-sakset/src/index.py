1ec32ec74aefb9e6ecd0eb515fb5581404b1e00d
Juuso Hyyryläinen <juusohyyrylainen@hotmail.com>
2023-12-19 23:28:45 +0200

    viikko7

---
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -0,0 +1,41 @@
+from kpsfactory import KiviPaperiSakset
+
+def main():
+    kpspeli = KiviPaperiSakset()
+    while True:
+        print("Valitse pelataanko"
+              "\n (a) Ihmistä vastaan"
+              "\n (b) Tekoälyä vastaan"
+              "\n (c) Parannettua tekoälyä vastaan"
+              "\nMuilla valinnoilla lopetetaan"
+              )
+
+        vastaus = input()
+
+        if vastaus.endswith("a"):
+            print(
+                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+            )
+
+            kaksinpeli = kpspeli.luo_pelaaja_vs_pelaaja()
+            kaksinpeli.pelaa()
+        elif vastaus.endswith("b"):
+            print(
+                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+            )
+
+            yksinpeli = kpspeli.luo_pelaaja_vs_tekoaly()
+            yksinpeli.pelaa()
+        elif vastaus.endswith("c"):
+            print(
+                "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
+            )
+
+            haastava_yksinpeli = kpspeli.luo_pelaaja_vs_edistynyt_tekoaly()
+            haastava_yksinpeli.pelaa()
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
